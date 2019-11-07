a57e8135b66f96d1cd0a0eb48df48036e7a8cdd3
Erik Oien <oieeri26@uw.edu>
2019-11-06 16:51:28 -0800

    added more transition stages
    it appears that there may be a problem with the state that is from sw_request to approved

--- referral_state_transitions.sql
+++ referral_state_transitions.sql
@@ -43,7 +43,7 @@ SELECT CASE
 	,"organizationId"
 	,min(sr."updatedAt") AS "date"
 	,'approved' AS "from"
-	,'requested' AS "to"
+	,'vc_received' AS "to"
 FROM staging."ServiceReferrals" AS sr
 LEFT JOIN staging."Organizations" AS o ON sr."organizationId" = o.id
 WHERE "requestDate" BETWEEN '2019-10-01'
@@ -55,20 +55,55 @@ WHERE "requestDate" BETWEEN '2019-10-01'
 		FROM staging."Organizations"
 		WHERE "routingOrg"
 		)
+	AND (
+		"initialVisitPlanId" = 1174068
+		OR "visitPlanId" = '1174068'
+		)
 GROUP BY "initialVisitPlanId"
 	,"visitPlanId"
 	,"organizationId"
 
+UNION
 
-SELECT DISTINCT "initialVisitPlanId"
+SELECT DISTINCT CASE 
+		WHEN "initialVisitPlanId" = 0
+			THEN "visitPlanId"::INTEGER
+		ELSE "initialVisitPlanId"
+		END AS "initialVisitPlanId"
 	,"visitPlanId"
 	,"OrganizationId"
-	,sr.id
-	,st.NAME
 	,DATE
-	,"StageTypeId"
-	,'' AS "from"
-	,'' AS "to"
+	,CASE 
+		WHEN "StageTypeId" = 7
+			THEN 'vc_received'
+		WHEN "StageTypeId" IN (
+				8
+				,12
+				)
+			THEN 'vc_received_timeline'
+		WHEN "StageTypeId" = 9
+			THEN 'accepted'
+		WHEN "StageTypeId" = 10
+			THEN 'visit_schedule_confirmed'
+		WHEN "StageTypeId" = 11
+			THEN 'not_sure'
+		ELSE ''
+		END AS "from"
+	,CASE 
+		WHEN "StageTypeId" = 7
+			THEN 'vc_received_timeline'
+		WHEN "StageTypeId" = 8
+			THEN 'accepted'
+		WHEN "StageTypeId" = 9
+			THEN 'visit_schedule_confirmed'
+		WHEN "StageTypeId" = 10
+			THEN 'first_visit_expected'
+		WHEN "StageTypeId" = 11
+			THEN 'resolved'
+		WHEN "StageTypeId" = 12
+			THEN 'rejected'
+		ELSE ''
+		END AS "to"
 FROM staging."ServiceReferrals" AS sr
 LEFT JOIN (
 	SELECT "ServiceReferralId"
@@ -80,8 +115,26 @@ LEFT JOIN (
 		,"StageTypeId"
 		,"OrganizationId"
 	) AS srts ON sr.id = srts."ServiceReferralId"
-LEFT JOIN staging."StageTypes" AS st ON srts."StageTypeId" = st.id
 WHERE "requestDate" BETWEEN '2019-10-01'
 		AND now()
 	AND "formVersion" = 'Ingested'
 	AND "organizationId" != 1
+
+UNION
+
+SELECT CASE 
+		WHEN "initialVisitPlanId" = 0
+			THEN "visitPlanId"::INTEGER
+		ELSE "initialVisitPlanId"
+		END AS "initialVisitPlanId"
+	,"visitPlanId"
+	,"organizationId"
+	,"deletedAt" AS "date"
+	,'vc_received_timeline' AS "from"
+	,'deleted' AS "to"
+FROM staging."ServiceReferrals"
+WHERE "requestDate" BETWEEN '2019-10-01'
+		AND now()
+	AND "formVersion" = 'Ingested'
+	AND "organizationId" != 1
+	AND "deletedAt" IS NOT NULL
